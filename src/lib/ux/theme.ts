0654c066aafd7946bb15cd0ab8c20d3b7364635f
Julián Álvarez <julian@supplyside.io>
2024-07-24 17:34:33 -0300

    updated colors files

--- src/lib/ux/theme.ts
+++ src/lib/ux/theme.ts
@@ -1,6 +1,7 @@
 import { Open_Sans, Ubuntu } from 'next/font/google'
 import { Theme, createTheme as createThemeInner } from '@mui/material/styles'
 import { Components, Mixins, PaletteMode, ThemeOptions } from '@mui/material'
+import { colors } from '@/lib/ux/colors'
 
 // TODO:
 //  - I think the gradients can be replaced with `elevation` and custom shadows
@@ -61,19 +62,6 @@ const opensans = Open_Sans({
   display: 'swap',
   preload: true,
 })
-
-export const colors = {
-  lightBackground: 'rgb(248, 249, 250)',
-  darkBackground: '#111111',
-  brandBlue: 'rgb(65, 154, 248)',
-  brandCyan: 'rgb(95, 207, 216)',
-  brandPurple: 'rgb(96, 63, 138)',
-  accentMagenta: 'rgb(151, 31, 160)',
-  accentOrange: 'rgb(237, 115, 72)',
-  black: '#000000',
-  white: '#ffffff',
-  red: '#D32F2F',
-} as const
 
 const fonts = {
   display: ubuntu,
