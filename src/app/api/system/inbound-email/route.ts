58fb1e5b3e38c57256ccc0dfb1530d9baa24b12b
Chris Kuech <chris@kuech.dev>
2024-07-25 17:36:47 -0700

    create bill from emails without attachments

--- src/app/api/system/inbound-email/route.ts
+++ src/app/api/system/inbound-email/route.ts
@@ -1,51 +1,106 @@
 'use server'
 
+import { fail } from 'assert'
 import { NextRequest, NextResponse } from 'next/server'
 import { Message } from 'postmark'
 import { createBlob } from '@/domain/blobs/actions'
 import prisma from '@/lib/prisma'
 import { createResource } from '@/domain/resource/actions'
+// import smtp from '@/lib/smtp'
+
+type Params = {
+  accountId: string
+  content: string
+  encoding: BufferEncoding
+  contentType: `${string}/${string}`
+  fileName: string
+}
+
+const createBill = async ({
+  accountId,
+  content,
+  encoding,
+  contentType,
+  fileName,
+}: Params) => {
+  const blob = await createBlob({
+    accountId,
+    buffer: Buffer.from(content, encoding),
+    type: contentType,
+  })
+
+  const file = await prisma().file.create({
+    data: {
+      accountId,
+      name: fileName,
+      blobId: blob.id,
+    },
+  })
+
+  return await createResource({
+    accountId,
+    type: 'Bill',
+    data: {
+      Document: file.id,
+    },
+  })
+}
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
   const body: Message = await req.json()
 
-  const accountKey = body.To?.split('@').shift()
+  const accountKey = body.To?.split('@').shift() ?? fail()
 
   const account = await prisma().account.findUnique({
     where: { key: accountKey },
   })
 
-  if (!account)
-    return NextResponse.json(
-      { error: 'Bad Request: Account does not exist' },
-      { status: 400 },
-    )
-
-  for (const attachment of body.Attachments ?? []) {
-    if (attachment.ContentType !== 'application/pdf') continue
-
-    const blob = await createBlob({
-      accountId: account.id,
-      buffer: Buffer.from(attachment.Content, 'base64'),
-      type: 'application/pdf',
-    })
-
-    const file = await prisma().file.create({
-      data: {
-        accountId: account.id,
-        name: attachment.Name,
-        blobId: blob.id,
-      },
-    })
-
-    await createResource({
-      accountId: account.id,
-      type: 'Bill',
-      data: {
-        Document: file.id,
-      },
-    })
+  if (!account) {
+    // await smtp().sendEmailWithTemplate({
+    //   From: 'bot@supplyside.io',
+    //   To: body.From,
+    //   Subject: "We couldn't process your email",
+    //   Attachments: body.Attachments,
+    // })
+    return NextResponse.json({ error: 'Account does not exist' })
   }
 
-  return NextResponse.json({ success: true })
+  const { id: accountId } = account
+  const attachment = body.Attachments?.find(
+    (a) => a.ContentType === 'application/pdf',
+  )
+
+  const params: Params | null = attachment
+    ? {
+        accountId,
+        content: attachment.Content,
+        encoding: 'base64',
+        contentType: 'application/pdf',
+        fileName: attachment.Name,
+      }
+    : body.HtmlBody
+      ? {
+          accountId,
+          content: body.HtmlBody,
+          encoding: 'utf-8',
+          contentType: 'text/html',
+          fileName: 'email.html',
+        }
+      : body.TextBody
+        ? {
+            accountId,
+            content: body.TextBody,
+            encoding: 'utf-8',
+            contentType: 'text/plain',
+            fileName: 'email.txt',
+          }
+        : null
+
+  if (!params) {
+    return NextResponse.json({ error: 'Failed to infer the type' })
+  }
+
+  const bill = await createBill(params)
+
+  return NextResponse.json({ success: true, bill })
 }
