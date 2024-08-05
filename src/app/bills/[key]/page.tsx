639f6460cb749f61c159b6295f94992d187fa9a7
Chris Kuech <chris@kuech.dev>
2024-08-05 09:39:21 -0700

    Bill page

--- src/app/bills/[key]/page.tsx
+++ src/app/bills/[key]/page.tsx
@@ -1,8 +1,12 @@
-import { Container, Stack, Typography } from '@mui/material'
+import { Box, Container, Stack, Typography } from '@mui/material'
 import { requireSessionWithRedirect } from '@/lib/session'
 import ResourceFieldsControl from '@/lib/resource/ResourceFieldsControl'
 import { readResource } from '@/domain/resource/actions'
 import { readSchema } from '@/domain/schema/actions'
+import CreateResourceButton from '@/lib/resource/CreateResourceButton'
+import ResourceTable from '@/lib/resource/ResourceTable'
+import ItemizedCostLines from '@/lib/resource/ItemizedCostLines'
+import { readResources } from '@/domain/resource/actions'
 
 export default async function BillsDetail({
   params: { key },
@@ -15,6 +19,17 @@ export default async function BillsDetail({
     readSchema({ accountId, resourceType: 'Bill' }),
   ])
 
+  const [lineSchema, lineResources] = await Promise.all([
+    readSchema({ accountId, resourceType: 'Line' }),
+    readResources({
+      accountId,
+      type: 'Line',
+      where: {
+        '==': [{ var: 'Bill' }, resource.id],
+      },
+    }),
+  ])
+
   return (
     <Container sx={{ my: 5 }}>
       <Stack spacing={5}>
@@ -24,6 +39,24 @@ export default async function BillsDetail({
             <span>{key}</span>
           </Typography>
           <ResourceFieldsControl schema={schema} resource={resource} />
+        </Stack>
+        <Stack spacing={2}>
+          <Stack direction={'row'} alignItems={'end'}>
+            <Typography variant="h4" flexGrow={1}>
+              Lines
+            </Typography>
+            <CreateResourceButton type={'Line'} data={{ Bill: resource.id }} />
+          </Stack>
+          <ResourceTable
+            schema={lineSchema}
+            resources={lineResources}
+            isEditable
+          />
+          <Stack direction={'row'} justifyContent={'end'}>
+            <Box width={'60%'}>
+              <ItemizedCostLines resource={resource} />
+            </Box>
+          </Stack>
         </Stack>
       </Stack>
     </Container>
