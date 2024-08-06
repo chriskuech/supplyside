5923e63a9f2fbe13ff92abe8bb30f3479b73e176
Chris Kuech <chris@kuech.dev>
2024-08-06 14:57:06 -0700

    Send PO email template

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 | -------------------------- | ------------------------------------------------ |
 | `nvm use --lts`            | Use the same nodejs version as our app.          |
 | `docker compose up`        | Run dependencies.                                |
+| `docker compose pull`      | Update dependencies.                             |
 | `npx prisma migrate reset` | (Re-)initialize the db and seed it.              |
 | `npx prisma generate`      | Generate new typings (without resetting the db). |
 | `npx prisma migrate dev`   | Generate a new db migration and apply it.        |
--- src/domain/order/sendPo.ts
+++ src/domain/order/sendPo.ts
@@ -4,6 +4,7 @@ import { fields } from '../schema/template/system-fields'
 import { selectValue } from '../resource/types'
 import smtp from '@/lib/smtp'
 import prisma from '@/lib/prisma'
+import config from '@/lib/config'
 
 type SendPoParams = {
   accountId: string
@@ -26,23 +27,48 @@ export const sendPo = async ({ accountId, resourceId }: SendPoParams) => {
 
   const poRecipient = selectValue(order, fields.poRecipient)?.contact
   const po = selectValue(order, fields.document)?.file
+  const assignee = selectValue(order, fields.assignee)?.user
+  const vendor = selectValue(order, fields.vendor)?.resource
+  const number = selectValue(order, fields.number)?.string
+  const date = selectValue(order, fields.issuedDate)?.date
 
   if (!po || !poRecipient?.email) return
 
-  const blob = await readBlob({ accountId, blobId: po.Blob.id })
+  const [poBlob, logoBlob] = await Promise.all([
+    readBlob({ accountId, blobId: po.Blob.id }),
+    account.logoBlobId
+      ? readBlob({ accountId, blobId: account.logoBlobId })
+      : undefined,
+  ])
 
-  if (!blob) return
+  if (!poBlob) return
 
-  await smtp().sendEmail({
-    From: 'bot@supplyside.io',
-    To: poRecipient.email,
-    Subject: 'New Purchase Order from ' + account.name,
-    TextBody: 'Please see attached purchase order.',
+  await smtp().sendEmailWithTemplate({
+    From: 'SupplySide <bot@supplyside.io>',
+    To: `${poRecipient.name} <${poRecipient.email}>`,
+    Cc: `${assignee?.fullName} <${assignee?.email}>`,
+    ReplyTo: `${assignee?.fullName} <${assignee?.email}>`,
+    TemplateAlias: 'new-po',
+    TemplateModel: {
+      // layout
+      buyer_logo_base64: logoBlob?.buffer.toString('base64'),
+      buyer_logo_contenttype: logoBlob?.mimeType,
+      buyer_company_name: account.name,
+      product_url: config().BASE_URL,
+
+      // template
+      supplier_user_name: poRecipient.name ?? '(No Name)',
+      buyer_user_name: assignee?.firstName ?? '(Unassigned)',
+      supplier_company_name: vendor?.name ?? '(No Vendor)',
+      order_number: number ?? '(No Number)',
+      date: date?.toLocaleDateString() ?? '(No Date)',
+    },
+    MessageStream: 'outbound',
     Attachments: [
       {
         Name: po.name,
         ContentID: '', // bad typings
-        Content: blob.buffer.toString('base64'),
+        Content: poBlob.buffer.toString('base64'),
         ContentType: po.Blob.mimeType,
       },
     ],
