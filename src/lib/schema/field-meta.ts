18caee7cdc6c25098b71b4afd83fc1c38bd597c0
Chris Kuech <chris@kuech.dev>
2024-08-08 21:28:36 -0700

    Fix Assignee

--- src/lib/schema/field-meta.ts
+++ src/lib/schema/field-meta.ts
@@ -1,14 +1,11 @@
 'use server'
 
-import { revalidateTag } from 'next/cache'
 import prisma from '@/lib/prisma'
 import { Option } from '@/domain/schema/types'
 import { requireSession } from '@/lib/session'
 
 export const readUsers = async (): Promise<Option[]> => {
   const { accountId } = await requireSession()
-
-  revalidateTag('iam')
 
   const users = await prisma().user.findMany({
     where: { accountId },
