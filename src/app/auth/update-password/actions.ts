7fbc6184fab0b286fa4cdab664abdef06631994c
Chris Kuech <chris@kuech.dev>
2024-08-16 10:15:35 -0700

    .

--- src/app/auth/update-password/actions.ts
+++ src/app/auth/update-password/actions.ts
@@ -2,14 +2,13 @@
 
 import { hash } from 'bcrypt'
 import { RedirectType, redirect } from 'next/navigation'
-import config from '@/lib/config'
 import prisma from '@/lib/prisma'
 import { requireSession } from '@/lib/session'
 
 export const updatePassword = async (password: string) => {
   const { userId } = await requireSession()
 
-  const passwordHash = await hash(password, config().SALT)
+  const passwordHash = await hash(password, 12)
 
   await prisma().user.update({
     where: { id: userId },
