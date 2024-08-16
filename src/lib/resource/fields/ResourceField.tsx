4e6f8dd0d75e6248dfeca4f96000aa446e960160
Julián Álvarez <julian@supplyside.io>
2024-08-16 11:31:26 -0300

    added spinner

--- src/lib/resource/fields/ResourceField.tsx
+++ src/lib/resource/fields/ResourceField.tsx
@@ -4,6 +4,7 @@ import assert from 'assert'
 import {
   Autocomplete,
   Box,
+  CircularProgress,
   Drawer,
   IconButton,
   Link,
@@ -119,7 +120,7 @@ function ResourceField(
                 singleColumn
               />
             ) : (
-              <></>
+              <CircularProgress />
             )}
           </Box>
         </Drawer>
