2fd0a464aafcc91b322702eb7932c90bced47721
Chris Kuech <chris@kuech.dev>
2024-08-16 16:20:20 -0700

    .

--- src/lib/allValues.tsx
+++ src/lib/allValues.tsx
@@ -1,10 +1,12 @@
-'use server';
+'use server'
+
 export const allValues = async <
-  T extends Readonly<Record<string, unknown>>
+  T extends Readonly<Record<string, unknown>>,
 >(promises: {
-  [K in keyof T]: Promise<T[K]> | T[K];
-}): Promise<T> => Object.fromEntries(
-  await Promise.all(
-    Object.entries(promises).map(async ([key, value]) => [key, await value])
-  )
-) as T;
+  [K in keyof T]: Promise<T[K]> | T[K]
+}): Promise<T> =>
+  Object.fromEntries(
+    await Promise.all(
+      Object.entries(promises).map(async ([key, value]) => [key, await value]),
+    ),
+  ) as T
