24d82ade4c6a9d730c29948f59658c607baa7360
Chris Kuech <chris@kuech.dev>
2024-08-16 16:10:31 -0700

    .

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,6 @@ import singleton from './singleton'
 const schema = z.object({
   AZURE_STORAGE_CONNECTION_STRING: z.string().min(1),
   BASE_URL: z.string().url(),
-  SESSION_KEY: z.string().min(1),
   NODE_ENV: z.enum(['development', 'integration', 'production']),
   POSTMARK_API_KEY: z.string().min(1),
 })
