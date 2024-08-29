bf0e02e5555d407606a42533769fc473315f73de
Chris Kuech <chris@supplyside.io>
2024-08-29 12:23:22 -0700

    Don't require quickbooks setup for local dev (#273)
    * Refactored to not require quickbooks
    
    * Bugfix deleting token
    
    * cleanup per julian's comment
    
    * Add back QuickBooksConnectedAt
    
    * lint fix
    
    ---------
    
    Co-authored-by: Julián Ignacio Alvarez <alvarezjulianignacio@gmail.com>

--- src/app/account/integrations/QuikBooksConnectButton.tsx
+++ src/app/account/integrations/QuikBooksConnectButton.tsx
@@ -1,26 +1,13 @@
 import { Button } from '@mui/material'
-import QuickbooksOauthClient from 'intuit-oauth'
-import Csrf from 'csrf'
-import config from '@/services/config'
-import { quickBooksClient } from '@/domain/quickBooks/client'
+import { createQuickBooksSetupUrl } from '@/domain/quickBooks/util'
 
 type Props = {
   accountId: string
 }
 
 export default async function QuickBooksConnectButton({ accountId }: Props) {
-  const state = {
-    accountId,
-    csrf: new Csrf().create(config().QUICKBOOKS_CSRF_SECRET),
-  }
-
-  const authUri = quickBooksClient().authorizeUri({
-    scope: [QuickbooksOauthClient.scopes.Accounting],
-    state: JSON.stringify(state),
-  })
-
   return (
-    <Button variant="outlined" href={authUri}>
+    <Button variant="outlined" href={createQuickBooksSetupUrl(accountId)}>
       Connect to QuickBooks
     </Button>
   )
