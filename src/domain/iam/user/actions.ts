f616c60c4b59f7a5a384b41bd8ab3d65e690aab7
Chris Kuech <chris@supplyside.io>
2024-08-30 12:35:54 -0700

    Fix bug with user select (same as that Assignee control bug) (#287)

--- src/domain/iam/user/actions.ts
+++ src/domain/iam/user/actions.ts
@@ -78,6 +78,7 @@ export async function readUsers({
     include: {
       ImageBlob: true,
     },
+    orderBy: [{ firstName: 'asc' }, { lastName: 'asc' }],
   })
 
   return users.map(mapUserModel)
