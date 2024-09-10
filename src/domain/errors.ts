801b691615295f780330274f91ecc1e3652dbfbe
Julián Álvarez <julian@supplyside.io>
2024-09-10 12:23:30 -0300

    fix

--- src/domain/errors.ts
+++ src/domain/errors.ts
@@ -1,7 +1,7 @@
 export class ExpectedError extends Error {}
 
 export const handleExpectedErrors =
-  <K>(fn: (params: K) => Promise<unknown>) =>
+  <K, T>(fn: (params: K) => Promise<T>) =>
   (params: K) =>
     fn(params).catch((e) => {
       if (e instanceof ExpectedError) {
