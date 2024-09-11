d235d1f8db400ac77d604e674ce8eb7447ed75f9
Chris Kuech <chris@kuech.dev>
2024-09-11 12:49:19 -0700

    Expose build info

---
+++ src/app/api/system/health/route.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { NextResponse } from 'next/server'
+import { z } from 'zod'
+import build from './build.json'
+
+const buildJsonSchema = z.object({
+  commit: z.string().regex(/^[0-9a-f]{40}$/),
+  version: z.coerce.number(),
+  timestamp: z.string().datetime(),
+})
+
+export async function GET(): Promise<NextResponse> {
+  return NextResponse.json({
+    timestamp: new Date().toISOString(),
+    build: buildJsonSchema.passthrough().parse(build),
+  })
+}
