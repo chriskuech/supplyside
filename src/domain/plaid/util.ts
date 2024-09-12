0d1f4a506e0341f0733e2d4d0b3c834e0958ae28
Julián Álvarez <julian@supplyside.io>
2024-09-12 08:52:24 -0300

    removed unused code

--- src/domain/plaid/util.ts
+++ src/domain/plaid/util.ts
@@ -36,13 +36,3 @@ export const plaidClient = () =>
       },
     }),
   )
-
-//TODO: change
-export const sessionOptions = {
-  cookieName: 'myapp_cookiename',
-  password: 'complex_password_at_least_32_characters_long',
-  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-  cookieOptions: {
-    secure: getPlaidConfigUnsafe().environment === 'production',
-  },
-}
