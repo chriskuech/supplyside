dc4dbf2ee2de8b2493040c79d8253a8c749ffa62
Chris Kuech <chris@kuech.dev>
2024-09-13 09:52:37 -0700

    Clone toast fix

--- src/lib/resource/detail/HandleJustCloned.tsx
+++ src/lib/resource/detail/HandleJustCloned.tsx
@@ -1,22 +1,21 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useSnackbar } from 'notistack'
 import { useEffect } from 'react'
 
 export default function HandleJustCloned() {
   const { enqueueSnackbar } = useSnackbar()
   const { replace } = useRouter()
-
-  const isCloned = new URL(location.href).searchParams.has('cloned')
+  const searchParams = useSearchParams()
 
   useEffect(() => {
-    if (isCloned) {
+    if (searchParams.has('cloned')) {
       enqueueSnackbar('Resource cloned', { variant: 'success' })
 
       replace(location.pathname)
     }
-  }, [enqueueSnackbar, isCloned, replace])
+  }, [enqueueSnackbar, replace, searchParams])
 
   return null
 }
