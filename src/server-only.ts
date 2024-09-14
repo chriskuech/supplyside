c8dc0daeee5217ef9701224a7d80325079a20483
Chris Kuech <chris@kuech.dev>
2024-09-13 21:46:42 -0700

    .

--- src/server-only.ts
+++ src/server-only.ts
@@ -1,3 +1,7 @@
-if (!process.argv.some((arg) => arg.endsWith('/prisma/seed/main.ts'))) {
+const isSeeding = process.argv.some((arg) =>
+  arg.endsWith('/prisma/seed/main.ts'),
+)
+
+if (!isSeeding) {
   throw new Error('This file should not be imported in the browser')
 }
