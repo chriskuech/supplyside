827316b583355209b95b26de35a2716bb6a32231
Chris Kuech <chris@supplyside.io>
2024-09-14 18:58:10 -0700

    QC OpenAI (#409)

--- src/domain/bill/extractData.ts
+++ src/domain/bill/extractData.ts
@@ -1,15 +1,7 @@
-import assert, { fail } from 'assert'
-import { mkdir, readFile, readdir, rm, writeFile } from 'fs/promises'
-import { exec as execCallback } from 'child_process'
-import { promisify } from 'util'
-import { P, match } from 'ts-pattern'
-import {
-  ChatCompletionContentPart,
-  ChatCompletionContentPartImage,
-  ChatCompletionContentPartText,
-} from 'openai/resources/index.mjs'
+import { fail } from 'assert'
 import { zodResponseFormat } from 'openai/helpers/zod'
 import { z } from 'zod'
+import { validate as isUuid } from 'uuid'
 import {
   ResourceFieldInput,
   readResource,
@@ -18,87 +10,70 @@ import {
 } from '../resource'
 import { selectResourceField } from '../resource/extensions'
 import { fields } from '../schema/template/system-fields'
-import { File } from '../files/types'
-import { selectSchemaField } from '../schema/extensions'
+import { selectSchemaFieldUnsafe } from '../schema/extensions'
 import { readSchema } from '../schema'
-import { readBlob } from '@/domain/blobs'
+import { mapFileToCompletionParts } from './mapFileToCompletionParts'
+import { mapVendorsToVendorList } from './mapVendorsToVendorList'
 import openai from '@/services/openai'
 
-const exec = promisify(execCallback)
-
 const prompt = `
-You are a context extraction tool within a "Procure-to-Pay" B2B SaaS application. Your task is to extract relevant information from uploaded bill/invoice documents. The documents may contain a mix of images, text, and HTML content. Your goal is to extract the Purchase Order (PO) number and the Vendor ID, if available. If the data is uncertain or ambiguous, do not include it in the output.
+You are a context extraction tool within a "Procure-to-Pay" B2B SaaS application.
+Your task is to extract relevant information from uploaded files associated with a Bill (aka Invoice).
+The documents may contain a mix of images, text, and HTML content and the actual Bill file may or may not be included.
+Your goal is to determine the Purchase Order (PO) number and the Vendor ID, if available; if the data is uncertain or ambiguous, do not include it in the output.
 
 You will be provided with the following context:
-- A list of vendors with their IDs.
-- The content of the uploaded bill/invoice documents.
+- A "Vendor List" TSV file containing Vendor IDs and Vendor Names.
+- The content of the uploaded documents associated with the Bill.
 
-Identify the Vendor by name in the bill/invoice documents and then lookup in the TSV vendor list provided by the user.
-
-All other files provided by the user are bill/invoice documents. Please respond with a JSON object containing the extracted information in the following format:
-{
-  "poNumber": "string or null",
-  "vendorId": "string or null"
-}
+You MUST only return high-confidence data. If the data is uncertain or ambiguous, do not include it in the output.
 `
 
-const ArgumentsSchema = z.object({
-  poNumber: z.string().nullish(),
-  vendorId: z.string().uuid().nullish(),
+const ExtractedBillDataSchema = z.object({
+  poNumber: z
+    .string()
+    .nullish()
+    .describe(
+      'The Purchase Order Number. This is a unique identifier for the Order associated with the Bill. If no PO Number is found in the Bill, this field should be null/missing.',
+    ),
+  vendorId: z
+    .string()
+    .nullish()
+    .describe(
+      'The Vendor ID. The Vendor ID is a UUIDv4 for identifying the Vendor. The Bill will contain the Vendor Name, not the Vendor ID. The Vendor ID must be looked up in the provided "Vendor List" TSV file by identifying the Vendor Name in the file (accounting for minor spelling/punctuation differences) and returning the associated Vendor ID for that Vendor Name. If no Vendor ID can be determined with high confidence, this field should be null/missing.',
+    ),
 })
 
 export const extractContent = async (accountId: string, resourceId: string) => {
-  const billSchema = await readSchema({
-    accountId,
-    resourceType: 'Bill',
-  })
+  const [billSchema, billResource, vendors] = await Promise.all([
+    readSchema({
+      accountId,
+      resourceType: 'Bill',
+    }),
+    readResource({
+      id: resourceId,
+      accountId,
+      type: 'Bill',
+    }),
+    readResources({
+      accountId,
+      type: 'Vendor',
+    }),
+  ])
 
-  const resource = await readResource({
-    id: resourceId,
-    accountId,
-    type: 'Bill',
-  })
-
-  const vendors = await readResources({
-    accountId,
-    type: 'Vendor',
-  })
-
-  const vendorList =
-    'Vendor List\n\n' +
-    [
-      {
-        id: 'ID',
-        name: 'Name',
-      },
-      ...vendors.map((vendor) => ({
-        id: vendor.id,
-        name: selectResourceField(vendor, fields.name)?.string,
-      })),
-    ]
-      .filter(({ name }) => !!name)
-      .map(({ id, name }) => `${id}\t${name}`)
-      .join('\n')
+  const vendorList = mapVendorsToVendorList(vendors)
 
   const billFiles =
-    selectResourceField(resource, fields.billFiles)?.files ??
+    selectResourceField(billResource, fields.billFiles)?.files ??
     fail('Files not found')
 
-  const completionParts: ChatCompletionContentPart[] = (
-    await Promise.all(
-      billFiles.map((file) =>
-        match(file.contentType)
-          .with('application/pdf', async () => await readPdfToBase64s(file))
-          .with(P.union('image/png', 'image/jpeg', 'image/webp'), async () => [
-            await readImageFileToBase64(file),
-          ])
-          .with(P.union('text/html', 'text/plain'), async () => [
-            await readTextFileToString(file),
-          ])
-          .otherwise(async () => []),
-      ),
-    )
+  if (!billFiles.length) return
+
+  const completionParts = (
+    await Promise.all(billFiles.map(mapFileToCompletionParts))
   ).flat()
+
+  if (!completionParts.length) return
 
   const completion = await openai().beta.chat.completions.parse({
     model: 'gpt-4o-2024-08-06',
@@ -118,7 +93,10 @@ export const extractContent = async (accountId: string, resourceId: string) => {
         ],
       },
     ],
-    response_format: zodResponseFormat(ArgumentsSchema, 'arguments'),
+    response_format: zodResponseFormat(
+      ExtractedBillDataSchema,
+      'extractedBillData',
+    ),
   })
 
   const data = completion.choices[0]?.message.parsed
@@ -126,24 +104,19 @@ export const extractContent = async (accountId: string, resourceId: string) => {
   const poNumber = data?.poNumber
   const vendorId = data?.vendorId
 
-  const poNumberFieldId =
-    selectSchemaField(billSchema, fields.poNumber)?.id ?? fail()
-  const vendorFieldId =
-    selectSchemaField(billSchema, fields.vendor)?.id ?? fail()
-
   const updatedFields: ResourceFieldInput[] = [
     ...(poNumber
       ? [
           {
-            fieldId: poNumberFieldId,
+            fieldId: selectSchemaFieldUnsafe(billSchema, fields.poNumber).id,
             value: { string: poNumber },
           },
         ]
       : []),
-    ...(vendorId
+    ...(vendorId && isUuid(vendorId)
       ? [
           {
-            fieldId: vendorFieldId,
+            fieldId: selectSchemaFieldUnsafe(billSchema, fields.vendor).id,
             value: { resourceId: vendorId },
           },
         ]
@@ -153,75 +126,4 @@ export const extractContent = async (accountId: string, resourceId: string) => {
   if (!updatedFields.length) return
 
   await updateResource({ resourceId, accountId, fields: updatedFields })
-}
-
-const readPdfToBase64s = async (
-  file: File,
-): Promise<ChatCompletionContentPartImage[]> => {
-  const blob = await readBlob({
-    accountId: file.accountId,
-    blobId: file.blobId,
-  })
-
-  assert(blob)
-
-  const containerPath = `/tmp/supplyside/${file.id}`
-  const inputPath = `${containerPath}/in.pdf`
-  const outputPath = `${containerPath}/out`
-
-  await mkdir(containerPath, { recursive: true })
-
-  try {
-    await writeFile(inputPath, blob.buffer)
-    await exec(`pdftoppm -png "${inputPath}" "${outputPath}"`)
-    const pngPaths = await readdir(`${outputPath}*`)
-    const base64s = await Promise.all(
-      pngPaths.map((pngPath) => readFile(pngPath, { encoding: 'base64' })),
-    )
-
-    return base64s.map((base64) => ({
-      type: 'image_url',
-      image_url: {
-        url: `data:image/png;base64,${base64}`,
-        detail: 'auto',
-      },
-    }))
-  } finally {
-    await rm(containerPath, { recursive: true, force: true })
-  }
-}
-
-const readImageFileToBase64 = async (
-  file: File,
-): Promise<ChatCompletionContentPartImage> => {
-  const blob = await readBlob({
-    accountId: file.accountId,
-    blobId: file.blobId,
-  })
-
-  assert(blob)
-
-  return {
-    type: 'image_url',
-    image_url: {
-      url: `data:${file.contentType};base64,${blob.buffer.toString('base64')}`,
-      detail: 'auto',
-    },
-  }
-}
-
-const readTextFileToString = async (
-  file: File,
-): Promise<ChatCompletionContentPartText> => {
-  const blob = await readBlob({
-    accountId: file.accountId,
-    blobId: file.blobId,
-  })
-
-  assert(blob)
-
-  return {
-    type: 'text',
-    text: blob.buffer.toString(),
-  }
 }
--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,6 +1,14 @@
 import { OpenAI } from 'openai'
 import singleton from './singleton'
 
-const openai = singleton('openai', () => new OpenAI())
+const openai = singleton(
+  'openai',
+  () =>
+    new OpenAI({
+      // These are required to use our specific model
+      organization: 'org-u3vWSqIyPWvKu4xGpMEcH5l4',
+      project: 'proj_RYX76NcxQN2NpLHkQuH7l33b',
+    }),
+)
 
 export default openai
