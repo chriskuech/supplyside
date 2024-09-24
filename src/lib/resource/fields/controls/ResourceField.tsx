28fdd7104baf7e12d79ce680fb6df9e185277ccd
Julián Ignacio Alvarez <alvarezjulianignacio@gmail.com>
2024-09-24 13:07:51 -0300

    bugfix: resource field grid edit mode cancelation (#477)

--- src/lib/resource/fields/controls/ResourceField.tsx
+++ src/lib/resource/fields/controls/ResourceField.tsx
@@ -186,13 +186,15 @@ const BaseEditableResourceField = (
         ...(inputValue ? [{ inputValue, name: `Add "${inputValue}"` }] : []),
       ]}
       getOptionLabel={(option) => option.name}
-      onChange={(event, newValue) =>
+      onChange={(event, newValue) => {
+        //Preventing propagation to avoid stopping the grid edit mode on enter key press
+        event.stopPropagation()
         match(newValue)
           .with({ id: P.string }, onUpdate)
           .with({ inputValue: P.string }, (o) => onCreate(o.inputValue))
           .with(null, () => {})
           .exhaustive()
-      }
+      }}
       onInputChange={(event, newInputValue) => setInput(newInputValue)}
       renderInput={(params) => (
         <Box display="flex" justifyContent="center" alignContent="center">
