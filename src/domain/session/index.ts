f4558ede515cb99bcfc4860328c98ebf95244c85
Chris Kuech <chris@kuech.dev>
2024-09-25 10:18:40 -0700

    fix di

--- src/domain/session/index.ts
+++ src/domain/session/index.ts
@@ -1,3 +1,4 @@
+import { singleton } from 'tsyringe'
 import { mapSessionModelToEntity } from './mappers'
 import { SessionCreationError } from './errors'
 import { sessionIncludes } from './model'
@@ -10,6 +11,7 @@ const SESSION_LIFESPAN_IN_DAYS = 7
 
 const lifespanInSeconds = 1000 * 60 * 24 * SESSION_LIFESPAN_IN_DAYS
 
+@singleton()
 export class SessionService {
   constructor(private readonly prisma: PrismaService) {}
 
