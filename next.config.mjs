029af7b3bf6cd8eb4b0c194833d580a7ab99c173
Julián Álvarez <julian@supplyside.io>
2024-09-25 09:46:55 -0300

    fixed handlebars warning

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      handlebars: 'handlebars/dist/handlebars.js',
+    }
+    return config
+  },
 }
 
 export default nextConfig
