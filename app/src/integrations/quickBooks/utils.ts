eab12a888faae25d5e8cc1a6a98261c49fce385c
Julián Álvarez <julian@supplyside.io>
2024-09-27 10:05:56 -0300

    PR comment

--- app/src/integrations/quickBooks/utils.ts
+++ app/src/integrations/quickBooks/utils.ts
@@ -1,13 +1,9 @@
+import { isMatching, P } from 'ts-pattern'
+
 export type RequestError = {
   response: {
     status: number
   }
 }
 
-export const isRequestError = (e: unknown): e is RequestError =>
-  !!e &&
-  typeof e === 'object' &&
-  'response' in e &&
-  !!e.response &&
-  typeof e.response === 'object' &&
-  'status' in e.response
+export const isRequestError = isMatching({ response: {status: P.number } })
