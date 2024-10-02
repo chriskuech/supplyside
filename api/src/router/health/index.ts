8c1667a13df86427daa2f1a7b2fd738c597b000f
Chris Kuech <chris@kuech.dev>
2024-10-01 18:13:11 -0700

    Fix app healthcheck

--- api/src/router/health/index.ts
+++ api/src/router/health/index.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { readFile } from 'fs/promises'
 
 export const mountHealth = async <App extends FastifyInstance>(app: App) =>
-  app.get('/health', async (request, reply) => {
+  app.get('/', async (request, reply) => {
     try {
       const meta = await readFile(`${__dirname}/build.json`, 'utf-8')
 
