47668afb14981ab9f73f12050122d910209d7d3d
Chris Kuech <chris@kuech.dev>
2024-10-08 15:29:52 -0700

    attempt shimming dirname

--- api/src/router/health/index.ts
+++ api/src/router/health/index.ts
@@ -1,5 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { readFile } from 'fs/promises'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const mountHealth = async <App extends FastifyInstance>(app: App) =>
   app.get('/', async (request, reply) => {
