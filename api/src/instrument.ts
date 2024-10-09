7882d23c49be21ea7909b3d46fa9fc0624b7062d
Chris Kuech <chris@kuech.dev>
2024-10-09 14:23:28 -0700

    Remove NODE_ENV from env

--- api/src/instrument.ts
+++ api/src/instrument.ts
@@ -2,7 +2,8 @@ import * as Sentry from '@sentry/node'
 import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
-  enabled: process.env.NODE_ENV !== 'development',
+  environment: process.env.SS_ENV,
+  enabled: process.env.SS_ENV !== 'development',
 
   dsn: 'https://889d72f14e15ac08f76d686aa96f5239@o4507972004610048.ingest.us.sentry.io/4508084675477504',
   integrations: [nodeProfilingIntegration()],
