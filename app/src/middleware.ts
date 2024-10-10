b54c7eaaef05fdb17d4c04c8d1a8c0f5273017ff
Chris Kuech <chris@kuech.dev>
2024-10-10 14:50:26 -0700

    PR feedback

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -2,36 +2,32 @@ import { NextResponse, type NextRequest } from 'next/server'
 import { readSession } from './session'
 import { readSelf } from './client/user'
 
-const PUBLIC_PAGES = ['/integrations/quickbooks/disconnected']
-
 export async function middleware(request: NextRequest) {
-  if (!PUBLIC_PAGES.includes(new URL(request.url).pathname)) {
-    // TODO: extend the session
-    const session = await readSession()
+  // TODO: extend the session
+  const session = await readSession()
 
-    if (!session)
-      return NextResponse.redirect(
-        new URL(`/auth/login?returnTo=${request.url}`, request.url),
-      )
+  if (!session)
+    return NextResponse.redirect(
+      new URL(`/auth/login?returnTo=${request.url}`, request.url),
+    )
 
-    const user = await readSelf(session.userId)
+  const user = await readSelf(session.userId)
 
-    if (!user)
-      return NextResponse.redirect(
-        new URL(`/auth/login?returnTo=${request.url}`, request.url),
-      )
+  if (!user)
+    return NextResponse.redirect(
+      new URL(`/auth/login?returnTo=${request.url}`, request.url),
+    )
 
-    if (!user.tsAndCsSignedAt)
-      return NextResponse.redirect(
-        new URL('/auth/terms-and-conditions', request.url),
-      )
-  }
+  if (!user.tsAndCsSignedAt)
+    return NextResponse.redirect(
+      new URL('/auth/terms-and-conditions', request.url),
+    )
 
   return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    '/((?!api|auth|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|icon.png|manifest.webmanifest).*)',
+    '/((?!api|auth|integrations|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|icon.png|manifest.webmanifest).*)',
   ],
 }
