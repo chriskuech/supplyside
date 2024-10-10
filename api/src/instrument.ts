25b2592bd28be1fbf98db78af34c61968f6ee100
Chris Kuech <chris@kuech.dev>
2024-10-09 20:47:13 -0700

    Tracing

--- api/src/instrument.ts
+++ api/src/instrument.ts
@@ -1,4 +1,8 @@
-import * as Sentry from '@sentry/node'
+import Sentry, {
+  fastifyIntegration,
+  prismaIntegration,
+  zodErrorsIntegration,
+} from '@sentry/node'
 import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 Sentry.init({
@@ -6,7 +10,12 @@ Sentry.init({
   enabled: process.env.SS_ENV !== 'development',
 
   dsn: 'https://889d72f14e15ac08f76d686aa96f5239@o4507972004610048.ingest.us.sentry.io/4508084675477504',
-  integrations: [nodeProfilingIntegration()],
+  integrations: [
+    nodeProfilingIntegration(),
+    prismaIntegration(),
+    fastifyIntegration(),
+    zodErrorsIntegration(),
+  ],
 
   // https://sentry.zendesk.com/hc/en-us/articles/23337524872987-Why-is-the-the-message-in-my-error-being-truncated
   maxValueLength: 8192,
