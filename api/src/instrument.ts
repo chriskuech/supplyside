53c60e905c5bf71a6ae671388571aed47f9f6c1c
Chris Kuech <chris@kuech.dev>
2024-10-09 17:12:23 -0700

    Stop truncating errors

--- api/src/instrument.ts
+++ api/src/instrument.ts
@@ -7,6 +7,10 @@ Sentry.init({
 
   dsn: 'https://889d72f14e15ac08f76d686aa96f5239@o4507972004610048.ingest.us.sentry.io/4508084675477504',
   integrations: [nodeProfilingIntegration()],
+
+  // https://sentry.zendesk.com/hc/en-us/articles/23337524872987-Why-is-the-the-message-in-my-error-being-truncated
+  maxValueLength: 8192,
+
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
