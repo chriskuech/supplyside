8ac84768a9215f5359685f3f2966bc92f2a3e3ee
Julián Álvarez <julian@supplyside.io>
2024-10-16 16:20:58 -0300

    charts accordion

--- app/src/app/(app)/jobs/charts/Charts.tsx
+++ app/src/app/(app)/jobs/charts/Charts.tsx
@@ -1,12 +1,20 @@
 'use client'
 
-import { Paper, Stack } from '@mui/material'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Paper,
+  Stack,
+  Typography,
+} from '@mui/material'
 import { MutableRefObject, useLayoutEffect, useState } from 'react'
 import {
   GridApiPro,
   gridFilteredSortedRowEntriesSelector,
 } from '@mui/x-data-grid-pro'
 import { Resource } from '@supplyside/model'
+import { ExpandMore } from '@mui/icons-material'
 import CashflowPieChart from './CashflowPieChart'
 import CashflowBarChart from './CashflowBarChart'
 
@@ -27,13 +35,22 @@ export default function Charts({ gridApiRef }: Props) {
   }, [gridApiRef])
 
   return (
-    <Stack direction="row" spacing={2} height={200}>
-      <Paper sx={{ flexGrow: 1 }}>
-        <CashflowPieChart resources={resources ?? []} />
-      </Paper>
-      <Paper sx={{ flexGrow: 2 }}>
-        <CashflowBarChart resources={resources ?? []} />
-      </Paper>
-    </Stack>
+    <Accordion defaultExpanded>
+      <AccordionSummary expandIcon={<ExpandMore />}>
+        <Typography variant="h6" gutterBottom>
+          Cashflow
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Stack direction="row" spacing={2} height={200}>
+          <Paper sx={{ flexGrow: 1 }}>
+            <CashflowPieChart resources={resources ?? []} />
+          </Paper>
+          <Paper sx={{ flexGrow: 2 }}>
+            <CashflowBarChart resources={resources ?? []} />
+          </Paper>
+        </Stack>
+      </AccordionDetails>
+    </Accordion>
   )
 }
