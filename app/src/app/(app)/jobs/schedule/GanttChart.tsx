6b7d2c946260e1acf3a79a5838cc87b09faa6541
Chris Kuech <chris@kuech.dev>
2024-10-22 22:04:33 -0700

    prune hover

--- app/src/app/(app)/jobs/schedule/GanttChart.tsx
+++ app/src/app/(app)/jobs/schedule/GanttChart.tsx
@@ -9,7 +9,6 @@ import {
   selectSchemaFieldUnsafe,
 } from '@supplyside/model'
 import dayjs, { Dayjs } from 'dayjs'
-import { useMemo, useRef, useState } from 'react'
 import utc from 'dayjs/plugin/utc'
 import { amber } from '@mui/material/colors'
 import { JobBar } from './JobBar'
@@ -34,53 +33,15 @@ export default function GanttChart({
   numDays,
   jobs,
   dim,
-  scrollOffset,
 }: Props) {
-  const ref = useRef<HTMLDivElement>(null)
-  const [mouse, setMouse] = useState<{
-    x: number
-    y: number
-    isDown: boolean
-  } | null>(null)
-  const coords = useMemo(() => {
-    if (!mouse || !ref.current) return null
-
-    const { left, top } = ref.current.getBoundingClientRect()
-
-    return {
-      x: mouse.x - left - scrollOffset,
-      y: mouse.y - top,
-    }
-  }, [mouse, scrollOffset])
-  const hover = useMemo(() => {
-    if (!coords) return null
-
-    const i = Math.floor(coords.x / dim)
-    const j = Math.floor(coords.y / dim)
-
-    return { i, j }
-  }, [coords, dim])
-
   return (
     <Box
-      ref={ref}
       position="relative"
       width={`${numDays * dim}px`}
       height={`${jobs.length * dim}px`}
       sx={{ outline: '1px solid divider' }}
       top={0}
       left={0}
-      onMouseMove={(e) =>
-        setMouse((state) => ({
-          x: e.clientX,
-          y: e.clientY,
-          isDown: state?.isDown ?? false,
-        }))
-      }
-      onMouseLeave={() => setMouse(null)}
-      onMouseDown={(e) =>
-        setMouse({ x: e.clientX, y: e.clientY, isDown: true })
-      }
     >
       <GanttChartGrid dim={dim} numRows={jobs.length} numCols={numDays} />
       {/* Today */}
@@ -138,30 +99,6 @@ export default function GanttChart({
                   },
                 })
               }
-            />
-          )
-        }
-
-        if (hover?.j === i) {
-          return (
-            <Box
-              key={'jobbarcandidate-' + job.id}
-              onClick={() =>
-                updateResourceField(job.id, {
-                  fieldId: selectSchemaFieldUnsafe(jobSchema, fields.startDate)
-                    .fieldId,
-                  valueInput: {
-                    date: startDate.add(hover.i, 'day').toISOString(),
-                  },
-                })
-              }
-              position="absolute"
-              top={hover.j * dim}
-              left={hover.i * dim}
-              width={`${dim * productionDays}px`}
-              height={`${dim}px`}
-              sx={{ outline: '1px solid cyan', outlineOffset: '-2px' }}
-              bgcolor="cyan"
             />
           )
         }
