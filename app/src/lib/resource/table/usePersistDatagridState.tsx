e7159c7b8bb5b79d2d81686a54126617ff2347d7
Julián Álvarez <julian@supplyside.io>
2024-11-01 14:57:45 -0300

    usePersistDataGridState fix

--- app/src/lib/resource/table/usePersistDatagridState.tsx
+++ app/src/lib/resource/table/usePersistDatagridState.tsx
@@ -1,27 +1,27 @@
 import { GridInitialState, useGridApiRef } from '@mui/x-data-grid-pro'
-import { useCallback, useLayoutEffect, useState } from 'react'
+import { useCallback, useLayoutEffect } from 'react'
 import 'client-only'
+import useLocalStorageState from '@/hooks/useLocalStorageState'
 
 export const usePersistDatagridState = (storageKey: string | undefined) => {
   const apiRef = useGridApiRef()
 
-  const [initialState, setInitialState] = useState<GridInitialState>()
+  const [initialState, setInitialState] = useLocalStorageState<
+    GridInitialState | undefined
+  >(storageKey, undefined)
 
   const saveStateToLocalstorage = useCallback(() => {
     if (!storageKey) return
 
     const currentState = apiRef.current.exportState()
-    localStorage.setItem(storageKey, JSON.stringify(currentState))
-  }, [apiRef, storageKey])
+    setInitialState(currentState)
+  }, [apiRef, storageKey, setInitialState])
 
   useLayoutEffect(() => {
-    if (!storageKey) return
-
-    const stateFromLocalStorage = localStorage.getItem(storageKey)
-    setInitialState(
-      stateFromLocalStorage ? JSON.parse(stateFromLocalStorage) : {},
-    )
-  }, [saveStateToLocalstorage, storageKey])
+    if (!initialState) {
+      setInitialState({})
+    }
+  }, [initialState, setInitialState])
 
   return {
     apiRef,
