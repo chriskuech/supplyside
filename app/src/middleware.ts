eb3c24537943237244fba05d532cc0c672549dac
Chris Kuech <chris@kuech.dev>
2024-11-08 10:26:19 -0800

    Bugfix file redirect

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -28,6 +28,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|auth|integrations|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|icon.png|manifest.webmanifest).*)',
+    '/((?!api|auth|integrations|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|icon.png|icon-[^.]+.png|manifest.webmanifest).*)',
   ],
 }
