be507d38704ed09e06214b6877f5d60d8d821a2c
Chris Kuech <chris@kuech.dev>
2024-11-19 16:29:57 -0800

    reuse existing code

--- app/src/app/(app)/job-schedule/gantt-chart/GanttChartEventBar.tsx
+++ app/src/app/(app)/job-schedule/gantt-chart/GanttChartEventBar.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material'
 import dayjs, { Dayjs } from 'dayjs'
-import { useCallback, useState } from 'react'
 import { GanttChartEvent } from './GanttChartItem'
-import { useEventListener } from '@/hooks/useEventListener'
 import { useDrag } from '@/hooks/useDrag'
 
 type Props = {
@@ -18,39 +16,22 @@ export const GanttChartEventBar = ({
   minDate,
   event: { id, onChange, startDate, days, children },
 }: Props) => {
-  const defaultLeft = dim * dayjs(startDate).diff(minDate, 'day')
-  const [dragStartX, setDragStartX] = useState<number | null>(null)
-  const [dragX, setDragX] = useState<number>(0)
-
   const {
     onMouseDown: onResizeStart,
-    // isDragging: isResizing,
+    isDragging: isResizing,
     delta: resizeDelta,
   } = useDrag({
     onDrop: ({ x }) =>
       onChange?.({ days: Math.max(1, days + Math.round(x / dim)) }),
   })
 
-  const onMouseMove = useCallback(
-    (e: MouseEvent) => {
-      if (dragStartX === null) return
-      setDragX(e.clientX - dragStartX)
-    },
-    [dragStartX],
-  )
-
-  useEventListener('mousemove', onMouseMove)
-  useEventListener('mouseup', () => {
-    if (dragStartX === null) return
-
-    onChange?.({
-      startDate: dayjs(startDate)
-        .add(Math.round(dragX / dim), 'day')
-        .toDate(),
-    })
-
-    setDragStartX(null)
-    setDragX(0)
+  const {
+    onMouseDown: onMoveStart,
+    isDragging: isMoving,
+    delta: moveDelta,
+  } = useDrag({
+    onDrop: ({ x }) =>
+      onChange?.({ startDate: dayjs(startDate).add(x, 'day').toDate() }),
   })
 
   return (
@@ -58,18 +39,13 @@ export const GanttChartEventBar = ({
       key={id}
       position="absolute"
       top={dim * index}
-      left={defaultLeft + dragX}
+      left={dim * dayjs(startDate).diff(minDate, 'day') + (moveDelta?.x ?? 0)}
       width={dim * days + (resizeDelta?.x ?? 0)}
       height={dim}
-      onMouseDown={(e) => {
-        if (!onChange) return
-        setDragStartX(e.clientX)
-      }}
-      sx={{
-        cursor: onChange ? 'grab' : 'default',
-      }}
+      onMouseDown={onMoveStart}
+      sx={{ cursor: onChange ? 'grab' : 'default' }}
     >
-      {children({ isDragging: !!dragStartX })}
+      {children({ isDragging: isMoving || isResizing })}
       <Box
         position="absolute"
         height="100%"
